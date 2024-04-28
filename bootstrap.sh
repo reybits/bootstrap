a8170c339fc1c70d4db3e7be264a498a3359817d
Andrey A. Ugolnik <andrey@ugolnik.info>
2024-04-28 18:24:08 +0200

    Initial bootstrap.sh added.

---
+++ bootstrap.sh
@@ -0,0 +1,178 @@
+#!/bin/sh
+# TODO: Check dirs and files existance
+
+
+# --- common vars --------------------------------------------------------------
+
+CWD=$(pwd)
+UNAME=$(uname -s)
+
+
+# --- list of configs to install -----------------------------------------------
+
+CONFIG_DIR=${XDG_CONFIG_HOME:-$HOME/.config}
+if [ ! -d $CONFIG_DIR ]; then
+    mkdir $CONFIG_DIR
+fi
+
+
+# CONFIGS_LIST=( alacritty bash lazygit nvim tmux vifm vim wezterm zsh )
+CONFIGS_LIST=( nvim tmux vifm vim wezterm zsh )
+
+
+# --- list of tools to install -------------------------------------------------
+TOOLS_LIST=( nvim tmux vifm zoxide fd ripgrep fzf 7zip )
+
+
+# --- install zsh --------------------------------------------------------------
+
+helpZshNotInstalled() {
+    echo ""
+    echo "-------------------------------------------------"
+    echo "|                                               |"
+    echo "|           Oh My Zsh isn't installed           |"
+    echo "|                                               |"
+    echo "-------------------------------------------------"
+
+    echo ""
+    echo "Next steps:"
+    echo "  sh -c \"\$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\""
+    echo "  exit"
+    echo "  source ~/.zshrc"
+
+    echo ""
+    echo "Then run 'bottstrap.sh' again."
+    echo ""
+}
+
+if [ -z ${ZSH+x} ]; then
+    helpZshNotInstalled
+    exit
+fi
+
+if [ ! -d "$ZSH" ]; then
+    helpZshNotInstalled
+    exit
+fi
+
+
+# --- zsh plugins --------------------------------------------------------------
+
+ZSH_CUSTOM=${ZSH_CUSTOM:-$ZSH/custom}
+
+echo ""
+echo "-------------------------------------------------"
+echo "|                                               |"
+echo "|         Installing Oh My Zsh plugins          |"
+echo "|                                               |"
+echo "-------------------------------------------------"
+
+zshPluginInstallUpdate() {
+    PLUG_URL=$1
+    PLUG_PATH=$ZSH_CUSTOM/$2
+    if [ ! -d $PLUG_PATH ]; then
+        echo "Cloning $2..."
+        git clone $PLUG_URL $PLUG_PATH
+    else
+        echo "Updating $2..."
+        cd $PLUG_PATH && git pull -p >/dev/null ; cd - >/dev/null
+    fi
+}
+
+# install PowerLevel10K theme
+zshPluginInstallUpdate "https://github.com/romkatv/powerlevel10k.git" "themes/powerlevel10k"
+
+# install zsh-autosuggestions
+zshPluginInstallUpdate "https://github.com/zsh-users/zsh-autosuggestions" "plugins/zsh-autosuggestions"
+
+# install zsh-syntax-highlighting
+zshPluginInstallUpdate "https://github.com/zsh-users/zsh-syntax-highlighting.git" "plugins/zsh-syntax-highlighting"
+
+
+# --- clone or update ----------------------------------------------------------
+
+echo ""
+echo "-------------------------------------------------"
+echo "|                                               |"
+echo "|           Cloning/Updating configs            |"
+echo "|                                               |"
+echo "-------------------------------------------------"
+
+for i in "${CONFIGS_LIST[@]}"
+do
+    cd $CWD
+    if [ ! -d "config-$i" ]; then
+        echo "Cloning config-$i..."
+        git clone git@bitbucket.org:andreyu/config-$i.git && \
+            cd "config-$i" && \
+            git submodule update --init
+    else
+        echo "Updating config-$i..."
+        cd "config-$i" && \
+            git pull -p >/dev/null && \
+            git submodule update --init >/dev/null
+    fi
+done
+
+cd $CWD
+
+
+# ---  make links --------------------------------------------------------------
+
+echo ""
+echo "-------------------------------------------------"
+echo "|                                               |"
+echo "|                Setting links                  |"
+echo "|                                               |"
+echo "-------------------------------------------------"
+
+for i in "${CONFIGS_LIST[@]}"
+do
+    ln -s -F -i "$CWD/config-$i/$i" $CONFIG_DIR
+done
+
+# make .zshrc link
+ln -s -i "$CWD/config-zsh/.zshrc" ~/.zshrc
+
+
+# --- install brew and tools ---------------------------------------------------
+
+if [ $UNAME = "Darwin" ] ; then
+    echo ""
+    echo "-------------------------------------------------"
+    echo "|                                               |"
+    echo "|       macOS detected, installing tools        |"
+    echo "|                                               |"
+    echo "-------------------------------------------------"
+
+
+    if ! command -v brew &> /dev/null ; then
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
+
+    for i in "${TOOLS_LIST[@]}"
+    do
+        brew install $i
+    done
+fi
+
+
+# --- print next steps ---------------------------------------------------------
+
+echo ""
+echo "-------------------------------------------------"
+echo "|                                               |"
+echo "|                 Almost done                   |"
+echo "|                                               |"
+echo "-------------------------------------------------"
+
+echo ""
+echo "Next steps:"
+echo "  source ~/.zshrc"
+
+echo ""
+echo "Then make this steps:"
+echo "1. Configure zsh theme: 'p10k configure'"
+echo "2. Run and configure tmux:  'tmux', press 'prefix + I' to install plugins."
+echo ""
+
